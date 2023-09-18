690d4e0ef57be0111090250f0f765f8b525a9696
Muhamad Rizal Soeid <Rizalsoeid@gmail.com>
2023-09-18 14:29:54 +0800

    edit-icon-fe

--- public/app.js
+++ public/app.js
@@ -1,68 +1,59 @@
 function getUsersByRole(role) {
-    return fetch(`api/user/${role}`)
-        .then(response => response.json());
+    return fetch(`api/user/${role}`).then((response) => response.json());
 }
 
 function deleteUser(userId) {
     return fetch(`api/user/${userId}`, {
-        method: 'DELETE',
+        method: "DELETE",
     });
 }
 
 function getUsername() {
-    return fetch(`api/username`)
-        .then(response => response.json());
+    return fetch(`api/username`).then((response) => response.json());
 }
 
 function displayUser(user) {
-    const userContainer = document.createElement('div');
-    userContainer.classList.add('user-container');
+    const userContainer = document.createElement("div");
+    userContainer.classList.add("user-container");
 
-    const userinfoContainer = document.createElement('div');
-    userinfoContainer.classList.add('userinfo-container');
+    const userinfoContainer = document.createElement("div");
+    userinfoContainer.classList.add("userinfo-container");
 
-    const userinfoNameContainer = document.createElement('div');
-    userinfoNameContainer.classList.add('userinfo-name-container');
+    const userinfoNameContainer = document.createElement("div");
+    userinfoNameContainer.classList.add("userinfo-name-container");
     userinfoNameContainer.innerText = `${user.first_name} ${user.last_name}`;
 
-    const userinfoUsernameContainer = document.createElement('div');
-    userinfoUsernameContainer.classList.add('userinfo-username-container');
+    const userinfoUsernameContainer = document.createElement("div");
+    userinfoUsernameContainer.classList.add("userinfo-username-container");
     userinfoUsernameContainer.innerText = user.username;
 
     userinfoContainer.appendChild(userinfoNameContainer);
     userinfoContainer.appendChild(userinfoUsernameContainer);
 
-    const editDeleteContainer = document.createElement('div');
-    editDeleteContainer.classList.add('edit-delete-container');
+    const deleteContainer = document.createElement("div");
+    deleteContainer.classList.add("delete-container");
 
-    const editIcon = document.createElement('img');
-    editIcon.src = 'edit-icon.svg';
-    editIcon.alt = 'edit icon';
-    editIcon.classList.add('edit-icon');
+    const deleteIcon = document.createElement("img");
+    deleteIcon.src = "delete-icon.svg";
+    deleteIcon.alt = "delete icon";
+    deleteIcon.classList.add("delete-icon");
 
-    const deleteIcon = document.createElement('img');
-    deleteIcon.src = 'delete-icon.svg';
-    deleteIcon.alt = 'delete icon';
-    deleteIcon.classList.add('delete-icon');
-
-    deleteIcon.addEventListener('click', () => {
-        if(confirm('Are you user want to delete this user?')) {
+    deleteIcon.addEventListener("click", () => {
+        if (confirm("Are you user want to delete this user?")) {
             deleteUser(user._id)
                 .then(() => {
                     userContainer.remove();
                 })
-                .catch(error => {
-                    console.error('Error deleting user: ', error);
-                })
+                .catch((error) => {
+                    console.error("Error deleting user: ", error);
+                });
         }
     });
 
-    editDeleteContainer.appendChild(editIcon);
-    editDeleteContainer.appendChild(deleteIcon);
+    deleteContainer.appendChild(deleteIcon);
 
     userContainer.appendChild(userinfoContainer);
-    userContainer.appendChild(editDeleteContainer);
+    userContainer.appendChild(deleteContainer);
 
-    document.getElementById('user-list').appendChild(userContainer);
-
+    document.getElementById("user-list").appendChild(userContainer);
 }
